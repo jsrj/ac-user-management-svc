19c275925aad4de1a9957eea1f4507cb2656d524
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-07 03:16:38 -0700

    added pre-check for existing user before adding a duplicate to db; added client endpoint for getting a user by username

--- api/routes/router.js
+++ api/routes/router.js
@@ -21,22 +21,52 @@ router.post(`/${process.env.AUTH0_HOOK}`, (req, res, next) => {
   const lastName = 'not-set';
   const { userID, username, contactInfo } = req.body
 
-  new User({ 
-    userID, 
-    username, 
-    firstName, 
-    lastName, 
-    contactInfo 
+  // First check that the user does not already exist
+  User.findOne({ username: username})
+  .exec()
+  .then(userExists => {
+    if (userExists) {
+
+      // Reject request
+      res.status(500).json({ message: 'User Already Exists'}).end()
+    } else {
+
+      // Create new base user profile
+      new User({ 
+        userID, 
+        username, 
+        firstName, 
+        lastName, 
+        contactInfo 
+      })
+      .save()
+      .then(() => {
+        console.log('--- New User Profile Created ---');
+        res.status(200).json({ message: 'User Data Generation Succeeded'}).end()
+      })
+      .catch((err) => {
+        console.log(err)
+        res.status(500).json({ message: 'User Data Generation Failed' }).end()
+      })
+    }
   })
-  .save()
-  .then(() => {
-    console.log('--- New User Profile Created ---');
-    res.status(200).json({ message: 'User Data Generation Succeeded'}).end()
-  })
-  .catch((err) => {
+  .catch(err => {
     console.log(err)
-    res.status(500).json({ message: 'User Data Generation Failed' }).end()
   })
+})
+
+router.get(`/${process.env.CLIENT}`, (req, res, next) => {
+  if (req.query.username) {
+    User.findOne({ username: req.query.username})
+    .exec()
+    .then(user => {
+      res.status(200).json(user).end()
+    }).catch(err => {
+      res.status(500).json(err).end()
+    })
+  } else {
+    res.status(400).json({ message: 'No username specified.'}).end()
+  }
 })
 
 module.exports = router
