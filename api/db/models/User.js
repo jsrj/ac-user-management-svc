d81e389cd422020cc874c954554df94fef9b1c37
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-07 15:54:29 -0700

    updated defaults for consistency; added displayname property; fixed casing on lastname

--- api/db/models/User.js
+++ api/db/models/User.js
@@ -3,46 +3,28 @@ const mongoose = require('mongoose')
 // Consts
 const Schema = mongoose.Schema
 
-/** -- Auth0 User Data Reference --
-@param {object} user - The user being created
-@param {string} user.id - user id
-@param {string} user.tenant - Auth0 tenant name
-@param {string} user.username - user name
-@param {string} user.email - email
-@param {boolean} user.emailVerified - is e-mail verified?
-@param {string} user.phoneNumber - phone number
-@param {boolean} user.phoneNumberVerified - is phone number verified?
-@param {object} user.user_metadata - user metadata
-@param {object} user.app_metadata - application metadata
-@param {object} context - Auth0 connection and other context info
-@param {string} context.requestLanguage - language of the client agent
-@param {object} context.connection - information about the Auth0 connection
-@param {object} context.connection.id - connection id
-@param {object} context.connection.name - connection name
-@param {object} context.connection.tenant - connection tenant
-@param {object} context.webtask - webtask context
-@param {function} cb - function (error, response)
-*/
-
 const UserSchema = new Schema({
 
 	userID: { type: String },
 	username: { type: String },
-
+	displayname: {
+		type: String,
+		default: 'not-set'
+	},
 	firstname: {
 		type: String,
-		default: 'anonymous'
+		default: 'not-set'
 	},
-	lastName: {
+	lastname: {
 		type: String,
-		default: 'anonymous'
+		default: 'not-set'
 	},
-	
+
 	contactInfo: {
 		email: {
 			address: {
 				type: String,
-				default: 'unknown'
+				default: 'not-set'
 			},
 			isVerified: {
 				type: Boolean,
@@ -52,7 +34,7 @@ const UserSchema = new Schema({
 		phone: { 
 			number: {
 				type: String,
-				default: '1-000-000-0000'
+				default: 'not-set'
 			},
 			isVerified: {
 				type: Boolean,
