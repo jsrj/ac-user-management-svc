482e273313226cfd1dced93d6949d236860a4479
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-06 23:46:23 -0700

    added Auth0 user data reference per their post-registration hook documentation

--- api/db/models/User.js
+++ api/db/models/User.js
@@ -3,8 +3,29 @@ const mongoose = require('mongoose')
 // Consts
 const Schema = mongoose.Schema
 
+/** Auth0 User Data Reference
+@param {object} user - The user being created
+@param {string} user.id - user id
+@param {string} user.tenant - Auth0 tenant name
+@param {string} user.username - user name
+@param {string} user.email - email
+@param {boolean} user.emailVerified - is e-mail verified?
+@param {string} user.phoneNumber - phone number
+@param {boolean} user.phoneNumberVerified - is phone number verified?
+@param {object} user.user_metadata - user metadata
+@param {object} user.app_metadata - application metadata
+@param {object} context - Auth0 connection and other context info
+@param {string} context.requestLanguage - language of the client agent
+@param {object} context.connection - information about the Auth0 connection
+@param {object} context.connection.id - connection id
+@param {object} context.connection.name - connection name
+@param {object} context.connection.tenant - connection tenant
+@param {object} context.webtask - webtask context
+@param {function} cb - function (error, response)
+*/
+
 const UserSchema = new Schema({
-	fname: { type: String },
+	userID: { type: String },
 	lname: { type: String },
 	username: { type: String },
 	email: { type: String },
