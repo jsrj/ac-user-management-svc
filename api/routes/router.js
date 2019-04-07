0a1bfb7b832740e56097ec707639f43a81b6b7b8
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-07 01:55:56 -0700

    added defaults in the event of an undefined value

--- api/routes/router.js
+++ api/routes/router.js
@@ -19,15 +19,15 @@ router.post(`/${process.env.AUTH0_HOOK}`, (req, res, next) => {
   
   console.log('\n');
   console.log('----- User Data: -----');
-  console.log(`User ID: ${req.body.userID}`);
-  console.log(`Username: ${req.body.username}`);
+  console.log(`User ID: ${req.body.userID || 'no-id'}`);
+  console.log(`Username: ${req.body.username || 'not-set'}`);
   console.log('----- User Data: -----');
   console.log('\n');
   console.log('----- Contact Info: -----');
-  console.log(`Email Address: ${req.body.contactInfo.email.address}`);
-  console.log(`Email Verified?: ${req.body.contactInfo.email.isVerified}`);
-  console.log(`Phone Number: ${req.body.contactInfo.phone.number}`);
-  console.log(`Phone Number Verified?: ${req.body.contactInfo.phone.isVerified}`);
+  console.log(`Email Address: ${req.body.contactInfo.email.address || 'not-set'}`);
+  console.log(`Email Verified?: ${req.body.contactInfo.email.isVerified || 'not-set'}`);
+  console.log(`Phone Number: ${req.body.contactInfo.phone.number || 'not-set'}`);
+  console.log(`Phone Number Verified?: ${req.body.contactInfo.phone.isVerified || 'not-set'}`);
   console.log('\n');
 
   res.json(responseObj);
