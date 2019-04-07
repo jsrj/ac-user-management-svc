f0fdc7c0843de2ba38043f928c3439257e0be8fc
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-07 01:36:32 -0700

    updated Request Recieved logging to use req.hostname instead of deprecated req.host

--- api/routes/router.js
+++ api/routes/router.js
@@ -17,7 +17,7 @@ router.post(`/${process.env.AUTH0_HOOK}`, (req, res, next) => {
     data: req.body
   };
   console.log(responseObj);
-  console.log(`Request Recieved From: ${req.host}`);
+  console.log(`Request Recieved From: ${req.hostname}`);
 
   res.json(responseObj);
 })
